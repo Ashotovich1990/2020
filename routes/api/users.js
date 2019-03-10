3b352c5fec47c8b388bb8effd37c63cab2ff21a8
David Park <xdavidpark@gmail.com>
2019-03-10 03:50:52 -0700

    Add user-auth with validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../../config/models/User');
+const User = require('../../models/User');
+const passport = require('passport');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+router.get('/current', passport.authenticate('jwt', {session: false}), (req,res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  })
+})
+
 router.post('/register', (req, res) => {
-    // Check to make sure nobody has already registered with a duplicate email
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     User.findOne({ username: req.body.username })
       .then(user => {
         if (user) {
-          // Throw a 400 error if the email address already exists
           return res.status(400).json({username: "A user has already registered with this username"})
         } else {
-          // Otherwise create a new user
           const newUser = new User({
             username: req.body.username,
             password: req.body.password
@@ -32,6 +48,47 @@ router.post('/register', (req, res) => {
       })
   })
 
-
+  router.post('/login', (req,res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+  
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    User.findOne({ username })
+      .then(user=> {
+        if (!user) {
+          return res.status(404).json({ username: "This user does not exist." });
+        }
+  
+        bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if(isMatch) {
+              const payload = {
+                id: user.id,
+                username: user.username,
+                
+              }
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                }
+              )
+            } else {
+              return res.status(400).json({ password: "Incorrect password" })
+            }
+            
+          })
+      })
+  })  
 
 module.exports = router;
