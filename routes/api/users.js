fdeb0f3cb7dfd216f5592368273a6b70af26b017
David Park <xdavidpark@gmail.com>
2019-03-10 12:27:15 -0700

    Update /current get route

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,13 +8,10 @@ const jwt = require('jsonwebtoken');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
-
 router.get('/current', passport.authenticate('jwt', {session: false}), (req,res) => {
   res.json({
     id: req.user.id,
-    handle: req.user.handle,
-    email: req.user.email
+    username: req.user.username
   })
 })
 
