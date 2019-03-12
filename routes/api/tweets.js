3013ab1114c72faa0a4372b771f41066026e537a
David Park <xdavidpark@gmail.com>
2019-03-12 01:39:52 -0700

    Upload chained sentiment API call

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const headers = require('../../config/keys_twitter.js');
 const OAuth = require('oauth');
 const request = require('request');
+const axios = require('axios');
+
 
 const config = {headers: headers};
 
@@ -23,6 +25,7 @@ router.get("/all", (req, res) => {
     )
     let tweets;
     let auth;
+    let url;
 
     oauth.get(
         URL,
@@ -35,32 +38,87 @@ router.get("/all", (req, res) => {
         .statuses.map(status => status.text);
 
         auth = "as7FTR2m6ycuNE38XWXwo4FyAsYW3jnWwZmvUGoR1ps";
+        url = 'https://apis.paralleldots.com/v3/sentiment';
 
-        request({
-            uri: 'https://apis.paralleldots.com/v3/sentiment',
-            method: "post",
-            multipart: [
-                
-                {"text": tweets},
-                {"api_key": auth}
-                
-            ],
-            
-        },
-        function(error, response, body) {
-            if (error) {
-                return console.error('upload failed:', error);
-            }
-            console.log('upload successful', body);
-        }
-        );
 
-        return res.json(tweets);     
+        request.post({
+            url: 'https://apis.paralleldots.com/v3/sentiment',
+            formData:
+            {
+                "Content-type": "application/json",
+                "text": tweets,
+                "api_key": auth
+                
+                }
+                
+            },
+        function optionalCallback(error, httpResponse,body) {
+        if (error) {
+            return console.error('upload failed:', error);
         }
-        )
+        return res.json(body);   
+
+        });
+    });
 });
 
 
 
 
 module.exports = router
+
+
+
+        // axios({
+        //     method: 'post',
+        //     url: url,
+        //     data: data
+        // }).then(data=> console.log(data))
+        //     .catch(err=>console.log(err))
+        
+        // return res.json(sentiment);     
+        // });
+// request.post({
+//     url: 'https://apis.paralleldots.com/v3/sentiment',
+//     formData:
+//     {
+        
+//         "TEXT": tweets,
+//         "API KEY": auth
+        
+//         }
+        
+//     },
+//     function optionalCallback(error, httpResponse,body) {
+//         if (error) {
+//             return console.error('upload failed:', error);
+//         }
+//         console.log('upload successful', body);
+
+//         });
+
+// let sentiment = () => request({
+//     uri: 'https://apis.paralleldots.com/v3/sentiment',
+//     method: "post",
+//     multipart: {
+//         chunked: false,
+//         data: [
+//             {
+//                 'content-type': 'application/json',
+//                 body: tweets,
+//             },
+//             {
+//                 'content-type': 'application/json',
+//                 body: auth,
+//             }
+//         ]
+//     }
+    
+// },
+// function(error, response, body) {
+//     if (error) {
+//         return console.error('upload failed:', error);
+//     }
+//     console.log('upload successful', body);
+// }
+// );
