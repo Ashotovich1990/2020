0158916227e1fb3346fd5407bae08f68f42f132c
Viviene De Guzman <vivienedeg@gmail.com>
2019-03-12 15:42:12 -0700

    Merge with master

---
+++ frontend/src/components/browse/browse_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { fetchTweets} from '../../actions/tweets_actions';
+import { receiveSearch } from '../../actions/search_actions';
+import Browse from './browse';
+
+const mapStateToProps = state => ({
+    searchTerm: state.search,
+    sentiment: state.entities.sentiments.sentiment
+})
+
+const mapDispatchToProps = dispatch => ({
+  receiveSearch: (tag) => dispatch(receiveSearch(tag)),
+  fetchTweets: (tag) => dispatch(fetchTweets(tag)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Browse);
