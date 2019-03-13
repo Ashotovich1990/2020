f3eb3eb341ee0b5a14ae438374b7cb3fca8d1e41
Aram Sargsyan <aramsargsyan9902gmail.com>
2019-03-13 09:54:56 -0700

    Add candidates in Search option

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import tweets from './tweets_reducer';
 import sentiments from './sentiments_reducer';
+import candidates from './candidates_reducer';
 
 const entitiesReducer = combineReducers({
     tweets,
     sentiments,
+    candidates,
 });
   
   export default entitiesReducer;
