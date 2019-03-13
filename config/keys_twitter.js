84e01dc0300d06174247add9f54cacb06d3cb1cf
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-03-12 19:34:14 -0700

    Deploy for heroku

--- config/keys_twitter.js
+++ config/keys_twitter.js
@@ -1,6 +1,5 @@
-module.exports = {
-  "consumer_key": "kBZdrNjvelR92GVOeKt1XLxIQ",
-  "secret_key": "0gu9kJVQ5wbdo7O6KJrgS5REiTTkTZfAdYr6bu8fTKz02yKUyZ",
-  "access_token": "959927125703737345-VVy1q1acWq0qdkjeUZyUV3D5hxgMFEI",
-  "secret_token": "mfOL9KxOkApEavEGviFXMO8ZiUVOLJEgF3eJ9xOxMnA0K",
-  }
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_twitter_prod');
+} else {
+  module.exports = require('./keys_twitter_dev');
+}
