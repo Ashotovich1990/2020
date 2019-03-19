fb4fc76c208a20099b2ca75761fa98f017f50944
ioqren <corriestoddard@gmail.com>
2019-03-19 15:58:54 -0700

    Keys stuff

---
+++ config/keys_twitter_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "consumer_key": process.env.consumer_key,
+    "secret_key": process.env.secret_key,
+    "access_token": process.env.access_token,
+    "secret_token": process.env.secret_token,
+}
