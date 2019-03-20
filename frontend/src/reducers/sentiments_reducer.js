3d1d5aab8e1d75bea8e75766767de5265d879724
Viviene De Guzman <vivienedeg@gmail.com>
2019-03-19 19:42:59 -0700

    Fix update on search page

--- frontend/src/reducers/sentiments_reducer.js
+++ frontend/src/reducers/sentiments_reducer.js
@@ -1,10 +1,13 @@
 import {RECEIVE_TWEETS} from '../actions/tweets_actions';
+import {RECEIVE_SEARCH} from '../actions/search_actions';
 
 const sentimentsReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_TWEETS:
-         return action.payload.data.sentiment
+         return action.payload.data.sentiment;
+        case RECEIVE_SEARCH:
+          return {};
         default:
          return state;
     }
--- frontend/src/reducers/tweets_reducer.js
+++ frontend/src/reducers/tweets_reducer.js
@@ -1,10 +1,13 @@
 import {RECEIVE_TWEETS} from '../actions/tweets_actions';
+import {RECEIVE_SEARCH} from '../actions/search_actions';
 
 const tweetsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_TWEETS:
-         return action.payload.data.tweets
+         return action.payload.data.tweets;
+        case RECEIVE_SEARCH:
+          return [];
         default:
          return state;
     }
