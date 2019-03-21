d7fa935d50753135556bbb3a2ea2ee7b649cdd6f
Viviene De Guzman <vivienedeg@gmail.com>
2019-03-20 17:59:40 -0700

    Reset search page on logout

--- frontend/src/reducers/search_reducer.js
+++ frontend/src/reducers/search_reducer.js
@@ -1,10 +1,13 @@
 import { RECEIVE_SEARCH } from '../actions/search_actions';
+import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
 
 const searchReducer = (state = "", action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SEARCH:
          return action.tag
+        case RECEIVE_USER_LOGOUT:
+          return "";
         default:
          return state;
     }
