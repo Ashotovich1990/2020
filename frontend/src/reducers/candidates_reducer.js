dee1f31e0c417fe52ed40fcbbf4610774c8959f0
David Park <xdavidpark@gmail.com>
2019-03-28 02:26:54 -0700

    Add verbage to splash

--- frontend/src/reducers/candidates_reducer.js
+++ frontend/src/reducers/candidates_reducer.js
@@ -1,9 +1,8 @@
 const candidateNames = 
     [
         "Donald Trump", "Bill Weld", "Bob Corker", "Larry Hogan", "John Kasich",
-        "Cory Booker", "Pete Buttigieg", "Julian Castro", "John Delaney", "Tulsi Gabbard", "Kirsten Gillibrand", "Kamala Harris", "John Hickenlooper", "Jay Inslee",
-        "Amy Klobuchar", "Bernie Sanders", "Elizabeth Warren", "Marianne Williamson", "Andrew Yang", "Andrew Gillum", "Stacey Abrams", "Joe Biden", "Bill de Blasio", "Beto O'Rourke",
-        "Adam Kokesh", "John McAfee", "Vermin Supreme", "Arvin Vohra", "Sedinam Kinamo", "Dario Hunter", "Ian Schlakman"
+        "Cory Booker", "Pete Buttigieg", "Julian Castro", "John Delaney", "Tulsi Gabbard", "Kirsten Gillibrand", "Kamala Harris", "Jay Inslee",
+        "Amy Klobuchar", "Bernie Sanders", "Elizabeth Warren", "Andrew Yang", "Andrew Gillum", "Stacey Abrams", "Joe Biden", "Bill de Blasio", "Beto O'Rourke", "John McAfee"
     ];
 
     const candidatesReducer = (state= candidateNames, action) => {
